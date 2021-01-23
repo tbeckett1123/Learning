6c64c4e02e039f0860793c3c613e0c562ac7d1a0
tbeckett1123 <Timothy.Beckett@gmail.com>
2021-01-23 06:25:48 -0600

    Angular Getting Started Lesson 5 Templates Interpolation, and Directive

--- Angular-GettingStarted-master/APM/src/app/app.module.ts
+++ Angular-GettingStarted-master/APM/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
