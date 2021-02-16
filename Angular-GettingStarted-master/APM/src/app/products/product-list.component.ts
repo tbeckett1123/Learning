b6e726f99a052f6229d20c383617e4545ebc02a2
tbeckett1123 <Timothy.Beckett@gmail.com>
2021-02-16 17:52:14 -0600

    Lesson 11 Navigation and Routing Basics

--- Angular-GettingStarted-master/APM/src/app/products/product-list.component.ts
+++ Angular-GettingStarted-master/APM/src/app/products/product-list.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 
-@Component({
-    selector: 'pm-products',
+@Component({    
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']  
 })
