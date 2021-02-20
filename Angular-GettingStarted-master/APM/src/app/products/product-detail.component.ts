1a24f101c2e3118fbf184a63ff4956b497bd1ae6
tbeckett1123 <Timothy.Beckett@gmail.com>
2021-02-19 23:49:04 -0600

    * Lesson 12 Navigation and Routing Basics * Homework - Finish Product Detail Page.

--- Angular-GettingStarted-master/APM/src/app/products/product-detail.component.ts
+++ Angular-GettingStarted-master/APM/src/app/products/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({  
   templateUrl: './product-detail.component.html',
@@ -9,26 +11,21 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit {
 
   pageTitle: string = 'Product Detail';
-  product: IProduct;
+  product: IProduct;  
 
   constructor(private route: ActivatedRoute,
-              private router: Router) { 
+              private router: Router,
+              private productService: ProductService) { 
 
   }
 
   ngOnInit(): void {
     let id = +this.route.snapshot.paramMap.get('id');
     this.pageTitle += `: ${id}`;
-    this.product = {
-      'productId': id,
-      'productName': 'Leaf Rake',
-      'productCode': 'GDN-001',
-      'releaseDate': 'March 19, 2019',
-      'description': 'Leaf rake with 48-inch wooden handle.',
-      'price': 19.95,
-      'starRating': 3.2,
-      'imageUrl': 'assets/images/leaf_rake.png'
-    }
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => alert(err)
+    });
   }
 
   onBack(): void {
